21a670087c0fc8f5475d1d2e776f4d2c727ff12c
hoangtrinh <hoangtrinh@admicro.vn>
2025-04-22 09:11:29 +0700

    commit

--- client/src/lib/AuthProvider.tsx
+++ client/src/lib/AuthProvider.tsx
@@ -26,5 +26,6 @@ export function useAuth() {
   if (!context) {
     throw new Error('useAuth must be used within an AuthProvider');
   }
-  return context;
+  return true; // context.isAuthenticated;
+//   return context;
 }
